bcdb357714edd0c9c5c9117778f92bfa3b278056
Cresil12 <Cresil12@gmail.com>
2017-04-05 00:54:22 +0545

    added composer.json

--- index.php
+++ index.php
@@ -1,11 +1,1 @@
- Volume in drive D has no label.
- Volume Serial Number is A0D6-1B3F
-
- Directory of D:\xampp\htdocs\heroku
-
-04/05/2017  12:38 AM    <DIR>          .
-04/05/2017  12:38 AM    <DIR>          ..
-04/05/2017  12:38 AM                 0 index.php
-04/05/2017  12:36 AM                 8 README.md
-               2 File(s)              8 bytes
-               2 Dir(s)  29,561,229,312 bytes free
+<?php echo "this is index page"; ?>
